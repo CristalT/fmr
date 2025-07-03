895736d520ef554df2638f952a31813ab86b12d8
Marcelo Forclaz <marcelo.forclaz@stensul.com>
2025-07-03 13:43:11 -0300

    refactor: update path handling and remove build file preparation step

--- inertia/composables/use_path.ts
+++ inertia/composables/use_path.ts
@@ -1,7 +1,8 @@
 const IMAGES_URL = 'uploads/images'
-const STATIC_PATH = 'public/static'
+const STATIC_PATH = 'static'
 
-export default function usePath(origin = '') {
+export default function usePath() {
+  const { origin } = window.location
   function imagePath(filename?: string): string {
     if (filename) return `${origin}/${IMAGES_URL}/${filename}?t=${new Date().getTime()}`
     return ''
